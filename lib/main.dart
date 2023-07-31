155ab64c830651dd4cf31a73980c2e7889af8c2d
LeeGunHoo <khan96517@gmail.com>
2023-07-31 21:22:28 +0900

    flutter practice

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body:
+      ),
+    ),
+  );
+}
+
+class HomeScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+  }
+}
